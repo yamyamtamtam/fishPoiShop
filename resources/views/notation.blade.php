890441ad5091b1c00205731d2809eaceabe0a7bd
山崎　圭祐 <7623@NT2020121401.local>
2022-12-07 11:51:56 +0900

    プライバシーポリシー、特定商取引追加

--- resources/views/notation.blade.php
+++ resources/views/notation.blade.php
@@ -3,7 +3,35 @@
 <section class="wrapper">
     <div class="inner">
         <h2 class="headlineBarWhite">特定商取引に基づく表記</h2>
-        <p class="caption mt30">準備中。商品が揃い、稼働までには作る。</p>
+        <dl class="tableList mt30">
+            <dt>販売責任者</dt>
+            <dd>山崎圭祐</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>メールアドレス</dt>
+            <dd>fishpoishop@gmail.com</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>商品の販売価格</dt>
+            <dd>各商品ページをご参照ください。</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>商品以外の必要料金</dt>
+            <dd>配送料（宅急便：〇〇円、メール便：□□円）</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>支払方法</dt>
+            <dd>クレジットカード決済・銀行振込</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>商品の引渡時期</dt>
+            <dd>ご注文日から7営業日以内に発送いたします。</dd>
+        </dl>
+        <dl class="tableList">
+            <dt>返品・交換</dt>
+            <dd>商品到着後14日以内に限り返品・交換が可能です。<br>
+                送料については、商品に欠陥がある場合には当方負担、お客様のご都合による返品・交換の場合にはお客様負担となります。</dd>
+        </dl>
     </div>
 </section>
 @endsection
