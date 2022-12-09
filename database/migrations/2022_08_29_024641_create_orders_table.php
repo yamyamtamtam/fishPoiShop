d471a65a5326112a15b1c0161c1a3e0ce15ef59f
山崎　圭祐 <7623@NT2020121401.local>
2022-12-09 18:05:52 +0900

    orderテーブルのmigratio追加n

--- database/migrations/2022_08_29_024641_create_orders_table.php
+++ database/migrations/2022_08_29_024641_create_orders_table.php
@@ -15,10 +15,17 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('status', 20)->comment('注文ステータス');
+            $table->string('status', 20)->comment('ステータス');
             $table->string('product_id', 128)->comment('商品id');
-            $table->decimal('prices', 10, 0)->comment('購入時点の合計金額');
+            $table->decimal('user_id', 10, 0)->comment('購入ユーザー');
+            $table->decimal('prices', 10, 0)->comment('購入時点の金額');
+            $table->decimal('count', 2, 0)->comment('購入個数');
             $table->dateTime('bought_data')->comment('購入日時');
+            $table->text('delivery_name')->comment('お届け先お名前');
+            $table->string('delivery_postal', 9)->comment('お届け先郵便番号');
+            $table->text('delivery_address')->comment('お届け先住所');
+            $table->string('delivery_tel', 16)->comment('電話番号');
+            $table->text('delivery_mail')->comment('メールアドレス');
             $table->timestamps();
         });
     }
